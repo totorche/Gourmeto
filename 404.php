063a3f2b62d0ddb39eeab0372835e3013500e154
Hervé Torche <herve.torche@fbw-one.com>
2012-07-04 17:11:56 +0200

    voilà...

---
+++ 404.php
@@ -0,0 +1,19 @@
+<head>
+  <?php header("HTTP/1.1 404 Not found"); ?>
+</head>
+<h2>Votre page n'a pas été trouvée sur <?php echo SITENAME; ?> - Erreur 404</h2>
+<div>
+  La page que vous recherchez n'a pas été trouvée sur <?php echo SITENAME; ?>.<br />
+  Il se peut que l'URL entrée soit erronée, que la page ait été déplacée ou qu'elle n'existe plus.<br />
+  Veuillez utiliser les liens ci-dessous pour vous rendre sur <?php echo SITENAME; ?>.
+  <br /><br />
+  Si vous deviez à nouveau rencontrer cette erreur, veuillez contacter par email notre <a href="mailto:herve@fbw-one.com?subject=Erreur 404 <?php echo SITENAME; ?>&cc=info@fbw-one.com" title="contacter la hotline">hotline</a>.
+  <br /><br /><br /><br />
+  
+  
+  Se rendre directement sur les pages principales de <?php echo SITENAME; ?> : 
+  <br /><br />
+  <ul>
+    <li>- <a href="<?php echo SITE_URL; ?>" title="Page d'accueil">Page d'accueil</a></li>
+  </ul>
+</div>
